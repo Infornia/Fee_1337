28ef62b48461b5294d9a0fe310ba0727e76d4230
Michael WILK <mwilk@e3r7p18.42.fr>
2015-12-12 19:52:07 +0100

    Hello

---
+++ fee_error.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fee_error.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwilk <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/12 17:53:17 by mwilk             #+#    #+#             */
+/*   Updated: 2015/12/12 19:50:48 by mwilk            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fee_1337.h"
+
+static int	check_neighbor(char **t, int x, int y)
+{
+	if ()
+}
+
+static int		error_tetri(char **t)
+{
+	int i;
+	int j;
+
+	j = 0;
+	i = 0;
+	while (j)
+	{
+		i = 0;
+		if 
+		while (i < 4)
+		{
+			if (!check_neighbor(t, i, j))
+				return (0);
+		}
+	}
+}
+
+int		error_block()
+{
+	int		i;
+	char	t[4][5];
+
+	while (get_next_line(fd, &line) > 1)
+	{
+		i++;
+	}
+}
