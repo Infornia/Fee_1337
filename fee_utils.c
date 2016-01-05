d26a0e1c8fef7befc6885a1ed5d2c5238e21c051
Michael WILK <mwilk@e2r2p7.42.fr>
2016-01-05 18:12:40 +0100

    Yo pop corn

--- fee_utils.c
+++ fee_utils.c
@@ -6,34 +6,34 @@
 /*   By: mwilk <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/12 16:48:08 by mwilk             #+#    #+#             */
-/*   Updated: 2015/12/12 18:42:13 by mwilk            ###   ########.fr       */
+/*   Updated: 2016/01/04 18:12:44 by mwilk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fee_1337.h"
 
-int     is_tetrichar(char c)
+int		is_tetrichar(char c)
 {
-    if (c == '.' || c == '#')
-        return (1);
-    return (0);
+	if (c == '.' || c == '#')
+		return (1);
+	return (0);
 }
 
-int     is_sharp(char c)
+int		is_sharp(char c)
 {
-    if (c == '#')
-        return (1);
-    return (0);
+	if (c == '#')
+		return (1);
+	return (0);
 }
 
-void    fee_blocks_del(t_data *d)
+void	fee_blocks_del(t_data *d)
 {
-    int i;
-    
-    i = 0;
-    while (i < d->nb_blocks)
-        tt_del_tab(d->blocks[i], 5);
-    free(d->blocks);
+	int i;
+
+	i = 0;
+	while (i < d->nb_blocks)
+		tt_del_tab(d->blocks[i], 5);
+	free(d->blocks);
 }
 
 void	get_max(t_data *d, char **t)
@@ -41,7 +41,7 @@ void	get_max(t_data *d, char **t)
 	int		i;
 	int		j;
 	int		cmp;
-	
+
 	j = -1;
 	cmp = d->nb_blocks * 4;
 	while (++j < cmp)
--- main.c
+++ main.c
@@ -6,23 +6,14 @@
 /*   By: mwilk <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/12 16:48:08 by mwilk             #+#    #+#             */
-/*   Updated: 2015/12/12 18:42:13 by mwilk            ###   ########.fr       */
+/*   Updated: 2016/01/04 19:08:50 by mwilk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fee_1337.h"
 
-static void	print_usage(int ac)
-{
-	if (ac != 2)
-		ft_putendl("\033[32m\tusage: ./fillit [File]\n\033[0m");
-	else
-		ft_putstr("\033[35m\n\t *****Welcome to Fee 1337*****\n\n\033[0m");
-}
-
 int		main(int ac, char **av)
 {
-	print_usage(ac);
 	if (ac == 2)
 		fee_1337(av[1]);
 	return (0);
